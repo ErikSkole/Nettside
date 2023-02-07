2df6bb8b36f151ca4faa0be26860ba6af43572f0
ErikSkole <e-erijoh6@iskule.no>
2023-02-07 11:44:52 +0100

    Fikk Corner til å funke med tanke på animasjon og sånt

--- Javascript/button.js
+++ Javascript/button.js
@@ -1,7 +1,24 @@
 const corner = document.getElementById("Corner");
 const homebutton = document.getElementById("btn");
 const button = document.getElementById("hbtn");
+const lnkdv = document.getElementById("lnk_dv");
+const link1 = document.getElementById("lnk1");
+const link2 = document.getElementById("lnk2");
+const link3 = document.getElementById("lnk3");
+const link4 = document.getElementById("lnk4");
+const link5 = document.getElementById("lnk5");
+const link6 = document.getElementById("lnk6");
+const link7 = document.getElementById("lnk7");
 
 homebutton.addEventListener("click", () => {
-corner.classList.toggle("homeanim"), button.classList.toggle("homebtnanim");
+
+    corner.classList.toggle("homeanim2"), button.classList.toggle("homebtnanim2"), corner.classList.toggle("homeanim"), button.classList.toggle("homebtnanim"), link1.classList.toggle("link_appearance"), link2.classList.toggle("link_appearance"), link3.classList.toggle("link_appearance"), link4.classList.toggle("link_appearance"), link5.classList.toggle("link_appearance"), link6.classList.toggle("link_appearance"), link7.classList.toggle("link_appearance");
 });
+
+homebutton.addEventListener("click", () => {
+    lnkdv.classList.toggle("lnkdv");
+});
+
+setTimeout(function(){
+    document.body.className="";
+},500);
